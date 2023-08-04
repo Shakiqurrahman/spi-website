1affde3e7903658469d9d1226b5bd0903c081084
learnwithancor <ancorpoint0@gmail.com>
2023-08-04 15:31:40 +0600

    commited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import './Header.css'
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-        <div className="logo">
-            <img src="images/sylhet-polytechnic-institute.png" alt="" />
-        </div>
-        <nav className="navbar">
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/event">Event</NavLink></li>
-                <li><NavLink to="/department">Department</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/result">Result</NavLink></li>
-                <li><NavLink to="/facilities">Facilities</NavLink></li>
-            </ul>
-        </nav>
-        <div className="login-btn">
-            <Link href="">LOG IN</Link>
-        </div>
+  const [active, setActive] = useState(false);
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="images/sylhet-polytechnic-institute.png" alt="" />
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/event">Event</NavLink>
+          </li>
+          <li>
+            <NavLink to="/department">Department</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/result">Result</NavLink>
+          </li>
+          <li>
+            <NavLink to="/facilities">Facilities</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="login-btn">
+        <Link href="">LOG IN</Link>
+      </div>
     </header>
-    );
+  );
 };
 
 export default Header;
